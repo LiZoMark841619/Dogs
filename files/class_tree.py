f780b6c5dcb6b743c2dd6bbcec4ec0fb2d3c295c
LiZoMark841619 <githubs001@gmail.com>
2024-04-24 13:54:34 +0200

    ok

--- files/class_tree.py
+++ files/class_tree.py
@@ -1,7 +1,8 @@
-def cls_tree(cls_, indent):
-    print(cls_, indent * 2)
-    for super_cls in cls_.__bases__:
-        print(cls_tree(super_cls, indent + 2))
+def cls_tree(cls, indent):
+    print(indent * '-', cls.__name__)
+    for super_cls in cls.__bases__:
+        cls_tree(super_cls, indent + 2)
     
 def instanceTree(instance):
-    return cls_tree(instance.__class__.__name__, 1)
+    print(f'Classtre of object {instance._asdict()}: ')
+    cls_tree(instance.__class__, 1)
--- files/dogs.py
+++ files/dogs.py
@@ -1,11 +1,15 @@
-from typing import NamedTuple
+from typing import Any, NamedTuple
 from class_tree import instanceTree
 
 class Dog(NamedTuple):
     name: str
     age: int
     color: str
+    
+    def __call__(self) -> Any:
+        return instanceTree(self)
 
-if __name__ == '__main':
+if __name__ == '__main__':
     my_dog = Dog('Killer', 12, 'brown')
     print(my_dog)
+    my_dog()
