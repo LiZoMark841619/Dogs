b82c4e475adbc3423b34115cdfa68cf30e55f748
LiZoMark841619 <githubs001@gmail.com>
2024-04-24 17:37:01 +0200

    ok

--- files/animals.py
+++ files/animals.py
@@ -1,24 +1,28 @@
 from abc import ABC
 from abc import abstractmethod
+from class_tree import instanceTree
 
 class Animal(ABC):
     num_of_instance = 0
     
-    def __init__(self, name: str, age: int, color: str) -> None:
+    def __init__(self, kind: str, name: str, age: int, color: str) -> None:
+        self.kind = kind
         self.name = name
         self.age = age
         self.color = color
         self.count()
     
     def __str__(self):
-        return f'{self.__class__.__name__}(name: {self.name}, age: {self.age}, color: {self.color})'
+        return f'{self.__class__.__name__}(name: {self.name}, age: {self.age})'
+    
+    def __call__(self) -> object:
+        return instanceTree(self)
     
     @classmethod
     def count(cls):
         cls.num_of_instance += 1
-        
-    
+
 if __name__ == '__main__':
-    my_animal_1 = Animal('LittleOne', 3, 'brown')
+    my_animal_1 = Animal('dog', 'LittleOne', 3, 'brown')
     print(my_animal_1)
     print(Animal.num_of_instance)
--- files/dogs.py
+++ files/dogs.py
@@ -1,17 +1,21 @@
 from animals import Animal
-from class_tree import instanceTree
 
 def factory(aklass, *args):
     return aklass(*args)
 
 class Dog(Animal):
     
-    def __call__(self) -> object:
-        return instanceTree(self)
+    def __init__(self, name: str, age: int, color: str) -> None:
+        super().__init__(self, name, age, color)
+        self.kind = 'dog'
+        Animal.count()
     
 if __name__ == '__main__':
     my_dog = Dog('Killer', 12, 'brown')
     print(my_dog)
     my_dog()
-    print(my_dog)
     print(Dog.num_of_instance)
+    one, two, three = factory(Dog, 'Killer', 15, 'white'), factory(Dog, 'Tiny', 2, 'black'), factory(Dog, 'Birdy', 2, 'yellow')
+    print(one, two, three)
+    print(Dog.num_of_instance)
+    print(Animal.num_of_instance)
