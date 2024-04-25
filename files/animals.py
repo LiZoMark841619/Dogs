f78367f47a6c2311da523879ee4f4dbfa8cee4fc
LiZoMark841619 <githubs001@gmail.com>
2024-04-25 08:54:15 +0200

    ok

--- files/animals.py
+++ files/animals.py
@@ -1,6 +1,14 @@
 from abc import ABC, abstractmethod
-from class_tree import instanceTree
 
+def cls_tree(cls, indent):
+    print(indent * '-', cls.__name__)
+    for super_cls in cls.__bases__:
+        cls_tree(super_cls, indent + 2)
+
+def instanceTree(instance):
+    print(f'Classtre of object {instance.__dict__}: ')
+    cls_tree(instance.__class__, 1)
+    
 class Animal(ABC):
     num_of_instance = 0
     
