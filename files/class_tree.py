01715da9290468e517e6b693c16a5fa7c2c24e45
LiZoMark841619 <githubs001@gmail.com>
2024-04-25 08:54:59 +0200

    ok

--- files/class_tree.py
+++
@@ -1,8 +0,0 @@
-def cls_tree(cls, indent):
-    print(indent * '-', cls.__name__)
-    for super_cls in cls.__bases__:
-        cls_tree(super_cls, indent + 2)
-    
-def instanceTree(instance):
-    print(f'Classtre of object {instance.__dict__}: ')
-    cls_tree(instance.__class__, 1)
